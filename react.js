bea1998d10c1480c7178a8adac9e5f893c3dd9d4
Simeon Pashley <simeon@ringalpha.com>
2019-03-08 09:28:00 +0000

    react extension

---
+++ react.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-magic-numbers */
+module.exports = {
+  extends: ['./index.js', '../../etc/eslint/rules/plugins/react.js'],
+  env: {
+    mocha: true,
+  },
+  rules: {
+    'react/display-name': 0,
+    'react/prefer-stateless-function': [1, { ignorePureComponents: true }],
+    'react/no-did-update-set-state': 0,
+    'jsx-a11y/href-no-hash': 0,
+    'jsx-a11y/label-has-for': 0,
+    'react/jsx-handler-names': 0,
+    'import/prefer-default-export': 0,
+    'no-return-assign': 0,
+    'flowtype/no-types-missing-file-annotation': 0,
+  }
+};
